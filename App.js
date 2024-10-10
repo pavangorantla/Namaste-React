b840554f46caff47b787b336fd73740d0505ec57
Pavan Gorantla <pavangorantla116@gmail.com>
2024-10-10 13:06:05 -0500

    igniting my app

--- App.js
+++ App.js
@@ -1,31 +1,7 @@
-//Nested html in React
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-{/* <div id ="parent">
-  <div id="child">
-    <h1>I'm h1 tag</h1>
-    <h2>I'm h1 tag</h2>
-  </div>
-
-<div id ="child2">
-  <h1>I'm h1 tag</h1>
-  <h2>I'm h1 tag</h2>
-</div>
-</div> */}
-
-/* const header = React.createElement("div",{ id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I'm an h10 tag"),
-      React.createElement("h2", {}, "I am an h12 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I'm an h11 tag"),
-      React.createElement("h2", {}, "I am an h12 tag"),
-        ]),
-     ]); */
-
-
-
-const parent = React.createElement("div",{ id: "parent" }, [
+const parent = React.createElement("div",{ id: "parent" }, [ 
     React.createElement("div", { id: "child" }, [
         React.createElement("h1", {}, "I'm an h1 tag"),
         React.createElement("h2", {}, "I am an h2 tag"),
@@ -37,16 +13,5 @@ const parent = React.createElement("div",{ id: "parent" }, [
        ]);
 
 
-
-// const heading = React.createElement(
-//   'h1',
-//   {id: "heading", xyz:"abc"},
-//   "Hello Pavan this how i learn how to code!"
-// );
-
-console.log(parent);
-
-/* creating an element with three props and then assigning that to DOM browser to the root and display them on the screen. Everything we will render inside the r
-oot what is root document.getElement by ID is the root */
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(header);
+root.render(parent);
