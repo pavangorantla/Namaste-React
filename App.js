1c96f6829414f6d24f52bb936fa094776a01c393
Pavan Gorantla <pavangorantla116@gmail.com>
2024-10-23 23:03:09 -0500

    Component Composition

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement("div",{ id: "parent" }, [ 
-    React.createElement("div", { id: "child" }, [
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I am an h2 tag"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I am an h2 tag"),
-          ]),
-       ]);
+const TitleComponent = () => (
+  <h1 className="head" tabIndex="1">
+    Hello Pavan using JSX Concept
+  </h1>
+);
 
+//React Functional Component and rendering it on the screen.
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+const number = 1000;
+
+const FooterComponent = () => (
+  <div id="container">
+    <h2>{number}</h2>
+    <TitleComponent />
+    <TitleComponent></TitleComponent>
+    <h1 className="heading">Footer Component1</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<FooterComponent />);
--- README.md
+++ README.md
@@ -19,3 +19,29 @@
 - HTTPs
 - Tree Shaking - remove unused code
 - Different dev and prod bundles
+
+//component composition Example.
+//What is Component Composition?
+Component composition in React refers to the practice of building complex user interfaces by combining smaller, reusable components. In this approach, a parent component can render child components within its structure.
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const TitleComponent = () => (
+  <h1 className="head" tabIndex="1">
+    Hello Pavan using JSX Concept
+  </h1>
+);
+
+//React Functional Component and rendering it on the screen.
+
+const FooterComponent = () => (
+  <div id="container">
+    <TitleComponent />
+    <h1 className="heading">Footer Component1</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<FooterComponent />);
